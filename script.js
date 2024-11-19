8a7a109367748b33e0027b68269617318be76141
lmxfred15 <faridlmq@gmail.com>
2024-11-19 14:19:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('[href="0"]').forEach(anchor => {
+    anchor.addEventListener(click, funtion (e)) ({
+        e:PreventDefault(),
+
+        document:querySelector(this.GetAttribute('href')). scrollIntoview({
+            behavior: 'smooth'
+        }),
+    });
+});
